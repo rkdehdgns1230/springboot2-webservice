777b3dd22be9767686a89ff3a08eb1ad03600c54
dongHun <actgong@gmail.com>
2023-08-25 15:19:51 +0900

    fix:  ->  두 번째 필드 가져오도록 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
-CURRENT_PID=$(ps -ef | grep $PROJECT_NAME | grep jar | awk '{print $1}')
+CURRENT_PID=$(ps -ef | grep $PROJECT_NAME | grep jar | awk '{print $2}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
